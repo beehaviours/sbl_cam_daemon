ca2b709cc5597c120753a186921d2664d0737703
Sebastien Druon <sebastien.druon@umontpellier.fr>
2025-10-03 12:20:37 +0200

    added full path to script

--- scripts/sbl_start_acqui.sh
+++ scripts/sbl_start_acqui.sh
@@ -12,6 +12,6 @@ cam="$2"
 ip=$(( 98 + 2*(module) + cam ))
 screen_name="M${module}C${cam}"
 
-screen -dmS $screen_name bash -c "sbl_cam_daemon \
+screen -dmS $screen_name bash -c "/usr/local/bin/sbl_cam_daemon \
 	-d 1 -w 1920 -h 1200 -f 30 -o /beegfs/superbeelive_data/ \
 	-m $module -c $cam 10.24.3.$ip"
